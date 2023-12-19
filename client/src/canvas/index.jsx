8b1cc92dbb277976f38a169f9a7f136d78002d36
sydney-rd <sydneydavid56@gmail.com>
2023-12-19 10:53:20 -0500

    updating indexjsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Enviroment, Center } from "@react-three/drei";
+// import { Enviroment, Center } from "@react-three/drei";
 
 const CanvasModel = () => {
   return <div>Canvas</div>;
