13cd67ce70125e35e2353f9066a281a9db0d994b
sydney-rd <sydneydavid56@gmail.com>
2023-12-19 11:27:24 -0500

    created a shirt 3d model

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-// import { Enviroment, Center } from "@react-three/drei";
+import { Center } from "@react-three/drei";
+import Shirt from "./Shirt";
+import Backdrop from "./Backdrop";
+import CameraRig from "./CameraRig";
+import { Environment } from "@react-three/drei";
 
 const CanvasModel = () => {
-  return <div>Canvas</div>;
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+
+      {/* <CameraRig>
+      <Backdrop /> */}
+      <Center>
+        <Shirt />
+      </Center>
+      {/* </CameraRig> */}
+    </Canvas>
+  );
 };
 
-export default Canvas;
+export default CanvasModel;
